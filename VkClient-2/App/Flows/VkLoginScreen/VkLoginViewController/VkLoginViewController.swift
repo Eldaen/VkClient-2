5c7d05d118f717caa2d50aad55be879561ddc272
Sizov Denis <sv_skatch@bk.ru>
2022-04-26 15:11:15 +0300

    File Structure + empty controller

---
+++ VkClient-2/App/Flows/VkLoginScreen/VkLoginViewController/VkLoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VkLoginViewController.swift
+//  VkClient-2
+//
+//  Created by Денис Сизов on 26.04.2022.
+//
+
+import UIKit
+
+// MARK: - VkLoginViewController
+final class VkLoginViewController: UIViewController {
+	override func viewDidLoad() {
+		print("hello VkClient-2")
+	}
+}
