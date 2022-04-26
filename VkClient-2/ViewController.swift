919bf1c6d6b9192ff2ab75532e19d13e49db9f02
Denis Sizov <sv_skatch@bk.ru>
2022-04-26 14:51:46 +0300

    Initial Commit

---
+++ VkClient-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkClient-2
+//
+//  Created by Денис Сизов on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
