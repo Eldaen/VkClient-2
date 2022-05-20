9f015a46e560b96499c2b45b9af73363061b1ccf
Sizov Denis <sv_skatch@bk.ru>
2022-05-21 00:22:26 +0300

    Added View + Cell view

---
+++ VkClient-2/App/Flows/SearchGroups/SearchGroupsViewController/SearchGroupsView/Cell/SearchGroupsCell.swift
@@ -0,0 +1,92 @@
+//
+//  SearchGroupsCell.swift
+//  VkClient-2
+//
+//  Created by Денис Сизов on 21.05.2022.
+//
+
+import UIKit
+
+/// Ячейка группы для контроллера SearchGroupsController
+final class SearchGroupsCell: UITableViewCell {
+	
+	//MARK: - Properties
+	
+	/// ID группы, которую ячейка отображает сейчас
+	var id: Int?
+	
+	/// Является ли членом этой группы
+	var isMember: Int?
+	
+	/// Название группы
+	var name: String?
+	
+	/// Название группы
+	private let groupName: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	/// Логотип группы
+	private let groupImage: UIImageView = {
+		let image = UIImageView()
+		image.translatesAutoresizingMaskIntoConstraints = false
+		image.contentMode = .scaleAspectFit
+		return image
+	}()
+	
+	// MARK: - Methods
+	
+	/// Меняет картинку, используется для замены после подгрузки из сети
+	func setImage(with image: UIImage) {
+		groupImage.image = image
+		self.groupImage.layoutIfNeeded()
+	}
+	
+	/// Конфигурируем ячейку для отображения группы
+	func configure(name: String, image: UIImage?, id: Int, isMember: Int) {
+		groupName.text = name
+		groupImage.image = image
+		self.id = id
+		self.isMember = isMember
+		self.name = name
+		
+		self.contentView.addSubview(groupName)
+		self.contentView.addSubview(groupImage)
+		
+		setupConstaints()
+		animate()
+	}
+	
+	/// Запуск анимацию ячейки
+	func animate() {
+		self.groupImage.alpha = 0
+		self.groupName.alpha = 0
+		
+		UIView.animate(withDuration: 0.3,
+					   delay: 0,
+					   options: [],
+					   animations: {
+			self.groupImage.alpha = 1
+			self.groupName.alpha = 1
+		})
+	}
+	
+}
+
+// MARK: - Private methods
+private extension SearchGroupsCell {
+	func setupConstaints() {
+		NSLayoutConstraint.activate([
+			groupName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+			groupName.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+			
+			groupImage.leadingAnchor.constraint(equalTo: groupName.trailingAnchor, constant: 20),
+			groupImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+			groupImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+			groupImage.widthAnchor.constraint(equalToConstant: 58),
+			groupImage.heightAnchor.constraint(equalTo: groupImage.widthAnchor, multiplier: 1.0),
+		])
+	}
+}
--- VkClient-2/App/Flows/SearchGroups/SearchGroupsViewController/SearchGroupsView/SearchGroupsView.swift
+++ VkClient-2/App/Flows/SearchGroups/SearchGroupsViewController/SearchGroupsView/SearchGroupsView.swift
@@ -10,4 +10,79 @@ import UIKit
 /// Вью для SearchGroupsViewController
 final class SearchGroupsView: UIView {
 	
+	// MARK: - Subviews
+	
+	/// Вью поиска
+	public let searchBar: UISearchBar = {
+		let searchBar = UISearchBar()
+		searchBar.frame = .zero
+		searchBar.searchBarStyle = UISearchBar.Style.default
+		searchBar.isTranslucent = false
+		searchBar.sizeToFit()
+		return searchBar
+	}()
+	
+	/// Таблица для найденных групп
+	public let tableView: UITableView = {
+		let tableView = UITableView()
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.backgroundColor = .white
+		return tableView
+	}()
+	
+	/// Индикатор загрузки
+	public let spinner: UIActivityIndicatorView = {
+		let spinner = UIActivityIndicatorView(style: .medium)
+		spinner.color = .black
+		return spinner
+	}()
+	
+	// MARK: - Init
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.configureUI()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		self.configureUI()
+	}
+}
+
+// MARK: - Private methods
+private extension SearchGroupsView {
+	
+	/// Конфигурирует UI
+	func configureUI () {
+		addSubviews()
+		setupSpinner()
+		setupConstraints()
+		setupTableView()
+	}
+	
+	/// Добавляет сабвью на основную вью
+	func addSubviews() {
+		self.addSubview(tableView)
+		tableView.tableHeaderView = searchBar
+	}
+	
+	/// Задаёт констрейнты таблице
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			tableView.topAnchor.constraint(equalTo: self.topAnchor),
+			tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+			tableView.leftAnchor.constraint(equalTo: self.leftAnchor),
+			tableView.rightAnchor.constraint(equalTo: self.rightAnchor),
+		])
+	}
+	
+	/// Конфигурирует спиннер загрузки
+	func setupSpinner() {
+		spinner.center = self.center
+	}
+	
+	func setupTableView() {
+		tableView.rowHeight = 80
+	}
 }
