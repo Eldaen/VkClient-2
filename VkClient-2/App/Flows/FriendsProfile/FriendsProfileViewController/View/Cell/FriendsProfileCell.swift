085583c91976e702a3bb94e317ae37f80054a2c5
Sizov Denis <sv_skatch@bk.ru>
2022-05-24 15:39:59 +0300

    Added CollectionView extension + fixed TableView extension

---
+++ VkClient-2/App/Flows/FriendsProfile/FriendsProfileViewController/View/Cell/FriendsProfileCell.swift
@@ -0,0 +1,8 @@
+//
+//  FriendsProfileCell.swift
+//  VkClient-2
+//
+//  Created by Денис Сизов on 23.05.2022.
+//
+
+import Foundation
---
+++ VkClient-2/App/Flows/FriendsProfile/FriendsProfileViewController/View/FriendsProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  FriendsProfileView.swift
+//  VkClient-2
+//
+//  Created by Денис Сизов on 23.05.2022.
+//
+
+import Foundation
