4f95edbe897cbbf1983d8e299279bf622eb3973d
Sizov Denis <sv_skatch@bk.ru>
2022-05-27 11:39:06 +0300

    Added coreData methods to My Groups

--- VkClient-2/App/BusinessLogic/Managers/DataStoreManager.swift
+++ VkClient-2/App/BusinessLogic/Managers/DataStoreManager.swift
@@ -15,6 +15,17 @@ protocol DataManagerInput: AnyObject {
 	/// Сохраняет данные групп
 	/// - Parameter groups: Массив моделей групп
 	func saveGroups(_ groups: [GroupModel])
+	
+	/// Загружает все группы
+	/// - Returns: Массив групп, который был в базе данных или nil
+	func getGroups() -> [GroupModel]?
+	
+	/// Удаляет все группы из базы данных
+	func deleteAllGroups()
+	
+	/// Удаляет из базы группу с указанным ID
+	/// - Parameter id: ID группы
+	func deleteGroup(with id: Int)
 }
 
 // MARK: - DataStoreManager
@@ -23,7 +34,7 @@ final class DataStoreManager: DataManagerInput {
 	// MARK: - Core Data stack
 	
 	lazy var persistentContainer: NSPersistentContainer = {
-		let container = NSPersistentContainer(name: "CoreData")
+		let container = NSPersistentContainer(name: "VkClient_2")
 		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			if let error = error as NSError? {
 				fatalError("Unresolved error \(error), \(error.userInfo)")
@@ -61,6 +72,46 @@ final class DataStoreManager: DataManagerInput {
 				try viewContext.save()
 			} catch let error {
 				print(error)
+			}
+		}
+	}
+	
+	func getGroups() -> [GroupModel]? {
+		let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CoreDataGroup")
+		if let groups = try? viewContext.fetch(fetchRequest) as? [CoreDataGroup],
+		   !groups.isEmpty {
+			var result = [GroupModel]()
+			for group in groups {
+				let group = GroupModel(
+					name: group.name ?? "",
+					image: group.image ?? "",
+					id: Int(group.id),
+					isMember: Int(group.isMember)
+				)
+				result.append(group)
+			}
+			return result
+		} else {
+			return nil
+		}
+	}
+	
+	func deleteAllGroups() {
+		let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CoreDataGroup")
+		if let groups = try? viewContext.fetch(fetchRequest) as? [CoreDataGroup] {
+			for group in groups {
+				viewContext.delete(group)
+			}
+		}
+	}
+	
+	func deleteGroup(with id: Int) {
+		let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CoreDataGroup")
+		fetchRequest.predicate = NSPredicate(format: "id = %@", id)
+		
+		if let group = try? viewContext.fetch(fetchRequest) as? [CoreDataGroup] {
+			if let firstGroup = group.first {
+				viewContext.delete(firstGroup)
 			}
 		}
 	}
